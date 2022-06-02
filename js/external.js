1a74457ab496e0cd9e8b4e0b42cc2553ed13d0aa
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-06-02 16:06:25 -0500

    functions.js

--- js/external.js
+++ js/external.js
@@ -1,69 +1,105 @@
 'use strict';
 
-console.log('Hello from external JavaScript')
+// console.log('Hello from external JavaScript')
+//
+//
+// alert('Welcome to my Website!')
+//     let response = prompt('What is your favorite color?');
+//
+//     while (response === '') {
+//         response = prompt('What is your favorite color?')
+//     }
+//     console.log(response);
+//
+//     alert(response + ' is a great color!')
 
 
-alert('Welcome to my Website!')
-    let response = prompt('What is your favorite color?');
+// alert('Welcome to the Movie rental section!');
+//
+// let CPD = 3;
+// var LM = prompt('How many days are you renting the Little Mermaid?');
+// console.log(LM);
+// var BB = prompt('How many days are you renting Brother Bear?');
+// console.log(BB);
+// var H = prompt('How many days are you renting Hercules?');
+// console.log(H);
+//
+//
+// var total = [parseFloat(LM) + parseFloat(BB) + parseFloat(H)] * CPD; //Why does this need brackets to work? Without them, the result is 11 instead of 27 for 3,5,1//
+// console.log(total)
+// alert('Your total will be' + ' ' + total + ' ' + 'dollars')
 
-    while (response === '') {
-        response = prompt('What is your favorite color?')
-    }
-    console.log(response);
+//alert('Big Tech Section')
 
-    alert(response + ' is a great color!')
+    //var GH = prompt('How many hours did you work this week for Google?')
+    //console.log(GH)
+//
+    //var GP = prompt('Google hourly rate?')
+    //console.log(GP)
+//
+    //var AH = prompt('How many hours did you work this week for Amazon?')
+    //console.log(AH)
+//
+    //var AP = prompt('Amazon hourly rate?')
+    //console.log(AP)
+//
+    //var FH = prompt('How many hours did you work this week for Facebook?')
+     //console.log(FH)
+//
+     //var FP = prompt('Facebook hourly rate?')
+     //console.log(FP)
+//
+     //var paymentforweek = (GH * GP) + (AH * AP) + (FH * FP)
+     //console.log(paymentforweek)
+//
+     //alert('The total earned this week is' + ' ' + paymentforweek + ' ' + 'dollars')
 
 
-alert('Welcome to the Movie rental section!')
-{
-    let CPD = 3;
-
-    var LM = prompt('How many days are you renting the Little Mermaid?')
-    console.log(LM);
-
-    var BB = prompt('How many days are you renting Brother Bear?')
-    console.log(BB);
-
-    var H = prompt('How many days are you renting Hercules?')
-    console.log(H);
+//alert('Class Enrollment')
+//
+//let timeinschedule = confirm('Is your schedule clear?')
+//
+//let roominclass = confirm('Does the class have room for you?')
+//
+//alert('it is' + (roominclass + timeinschedule) + "You can attend this class")
 
 
-    var total = [parseInt(LM) + parseInt(BB) + parseInt(H)] * CPD; //Why does this need brackets to work? Without them, the result is 11 instead of 27 for 3,5,1//
-    console.log(total)
-    alert('Your total will be' + ' ' + total + ' ' + 'dollars')
+  //r cf = confirm('Is class full?')
+  //  console.log(cf)
+  //(confirm) true
+  //  alert('Not enrolled')
+  //  console.log(confirm);
+  //(confirm) false
+  //  var sc = confirm('Is there a schedule conflict?')
+  //      console.log(sc)
+  //  if(sc) true
+  //      alert('Not enrolled')
+  //  if(confirm) false
+  //      alert('Enrolled')
+  //      console.log(confirm)
 
-}
-alert('Big Tech Section') {
+let moreThanTwo = confirm('Do you have more than two items in your cart?')
+let offerStillValid = confirm('Is the offer on the product still valid?')
+let premiumMember = confirm('Are you a premium member?')
+alert( 'Discount?' + ' ' + (moreThanTwo && offerStillValid || premiumMember))
+    if(true) {alert("You get the discount")} else {"You don't get the discount"}
 
-    var GH = prompt('How many hours did you work this week for Google?')
-    console.log(GH)
 
-    var GP = prompt('Google hourly rate?')
-    console.log(GP)
 
-    var AH = prompt('How many hours did you work this week for Amazon?')
-    console.log(AH)
 
-    var AP = prompt('Amazon hourly rate?')
-    console.log(AP)
 
-    var FH = prompt('How many hours did you work this week for Facebook?')
-    console.log(FH)
 
-    var FP = prompt('Facebook hourly rate?')
-    console.log(FP)
 
-    var paymentforweek = (GH * GP) + (AH * AP) + (FH * FP)
-    console.log(paymentforweek)
 
-    alert('The total earned this week is' + ' ' + paymentforweek + ' ' + 'dollars')
-}
 
-alert('Class Enrollment') {
 
-    var
 
-}
+
+
+
+
+
+
 
 
 

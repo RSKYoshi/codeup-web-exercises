3dda86081594e7a4e573d0814b285d077d91d631
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-06-07 12:21:02 -0500

    loop progress

---
+++ js/for_loops.js
@@ -0,0 +1,31 @@
+"use strict";
+
+function showMultiplicationTable(x) {
+    for (var i = 1; i<11; i++){
+        const result = (i*x)
+        console.log(x  + "*" + i + "=" + result)
+    }
+}
+
+//Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even
+
+
+// var random = Math.floor((Math.random() * 200) + 20);
+// for (var i = 1;random; i++,  j--) {
+//     console.log("This number is")
+// }
+
+
+    for (var i = 1; i < 10; i++) {
+        for (var j = 1; j<=i; j++) {
+            console.log(i);
+        }
+    }
+
+
+
+
+
+
+
+

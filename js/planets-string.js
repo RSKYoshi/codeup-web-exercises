742bc5ff7ab8ecb8a9c67ca5d9b57cd4b3dad655
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-06-09 21:07:06 -0500

    planets and split-join

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,47 +1,73 @@
 (function(){
     "use strict";
 
-    var planets = [
-        'Mercury',
-        'Venus',
-        'Earth',
-        'Mars',
-        'Jupiter',
-        'Saturn',
-        'Uranus',
-        'Neptune'
-    ];
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+
 
     /**
      * TODO:
-     * Read each console log below, and write some javascript code to perform
-     * the step that it describes
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
      */
 
-    console.log('Adding "The Sun" to the beginning of the planets array.');
-    planets.unshift("The Sun");
-    console.log(planets);
+    var planetsArray = planetsString.split("|")
+    console.log(planetsArray)
 
-    console.log('Adding "Pluto" to the end of the planets array.');
-    planets.push("Pluto");
-    console.log(planets);
 
-    console.log('Removing "The Sun" from the beginning of the planets array.');
-    planets.shift()
-    console.log(planets);
+    //example from Kenneth
+    // var planetsArray = planetsString.split("|")
+    // console.log(planetsArray);
 
-    console.log('Removing "Pluto" from the end of the planets array.');
-    planets.pop();
-    console.log(planets);
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful? //now I can target each planet individually
+     * BONUS:
+     * Create another string that would display your planets in an unordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
 
-    console.log('Finding and logging the index of "Earth" in the planets array.');
-    console.log(planets.indexOf("Earth"))
+//this is the to do Q
+// var planetString = planetsArray.join("<br>")
+//     console.log(planetString);
 
-    console.log("Reversing the order of the planets array.");
-    planets.reverse()
-    console.log(planets);
 
-    console.log("Sorting the planets array.");
-    planets.sort()
-    console.log(planets);
+
+//this is the Bonus Q
+
+    var planetString = "<ul> <li>Mercury</li> <li>Venus</li> <li>Earth</li> <li>Mars</li> <li>Jupiter</li> <li>Saturn</li> <li>Uranus</li> <li>Neptune</li> </ul>"
+    console.log(planetString);
+
+var bonusQ =  planetString
+    document.write(bonusQ)
+
+
+
+
+// planetsArray += "<li>" + planetsArray.name + "</li>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //example from Kenneth
+    // let breaktagPlanets = planetsArray.join("<br>")
+    // console.log(breaktagPlanets);
 })();

ce3d413398c88c153050dc3b4f37eedcce5bc07a
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-07-14 13:08:08 -0500

    styled mapbox, just need to finish array

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -2,8 +2,6 @@
 
 /***
  * geocode is a method to search for coordinates based on a physical address and return
- * @param {string} search is the address to search for the geocoded coordinates
- * @param {string} token is your API token for MapBox
  * @returns {Promise} a promise containing the latitude and longitude as a two element array
  *
  * EXAMPLE:
@@ -13,10 +11,10 @@
  *  })
  *
  */
-function geocode(search, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+function geocode(address, token= mapBoxApiKey ) {
+    const baseUrl = 'https://api.mapbox.com';
+    const endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(address) + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
             return res.json();
             // to get all the data from the request, comment out the following three lines...
